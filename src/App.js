aab5461c87469deef74a6aa060b386a000d78493
Abdelrahman_Rashad <a.rashaad98@gmail.com>
2023-01-05 14:44:41 +0200

    add news body container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header/header.component';
+import BodyContainer from './components/body-container/body-container.component';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <BodyContainer />
     </div>
   );
 }
---
+++ src/components/body-container/body-container.component.jsx
@@ -0,0 +1,29 @@
+
+
+const BodyContainer = () => {
+    return (
+        <div className="container-fluid mt-5 pt-5">
+            <div className="row">
+                <div className="col-8">
+                    The Bright Future of Web 3.0
+                </div>
+                <div className="col-4">
+                    New
+                </div>
+                <div className="row">
+                    <div className="col">
+                        Reviving Retro PCs
+                    </div>
+                    <div className="col">
+                        Top 10 Laptops of 2020
+                    </div>
+                    <div className="col">
+                        The Growth of Gaming
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default BodyContainer;
