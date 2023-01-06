34834c14bfb4cd8fe27a5955b2bfd2353882d9b4
Abdelrahman_Rashad <a.rashaad98@gmail.com>
2023-01-06 02:14:56 +0200

    configure the body container component for small screen view

--- src/components/body-container/body-container.component.jsx
+++ src/components/body-container/body-container.component.jsx
@@ -4,20 +4,20 @@ const BodyContainer = () => {
     return (
         <div className="container-fluid mt-5 pt-5">
             <div className="row">
-                <div className="col-8">
+                <div className="col-lg-8 ">
                     The Bright Future of Web 3.0
                 </div>
-                <div className="col-4">
+                <div className="col-lg-4">
                     New
                 </div>
                 <div className="row">
-                    <div className="col">
+                    <div className="col-lg">
                         Reviving Retro PCs
                     </div>
-                    <div className="col">
+                    <div className="col-lg">
                         Top 10 Laptops of 2020
                     </div>
-                    <div className="col">
+                    <div className="col-lg">
                         The Growth of Gaming
                     </div>
                 </div>
