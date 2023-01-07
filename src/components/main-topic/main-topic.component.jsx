fd318afa416f03eaa3c06e5043dcb06fa3581c8b
Abdelrahman_Rashad <a.rashaad98@gmail.com>
2023-01-07 07:28:28 +0200

    update main topic component styles

--- src/components/main-topic/main-topic.component.jsx
+++ src/components/main-topic/main-topic.component.jsx
@@ -10,13 +10,18 @@ const MainTopic = () => {
                 </div>
             </div>
             <div className="row">
-                <h1 className="col gy-4">
+                <h1 className="col-lg-6 gy-4">
                     The Bright Future of Web 3.0?
                 </h1>
-                <div className="col gy-4">
+                <div className="col-lg-6 gy-4">
                     <p>
                         We dive into the next evolution of the web that claims to puth the power of the platforms back into the hands of the people. But is it really fulfilling its promise?
                     </p>
+                    <div className='row'>
+                    <button>
+                        READ MORE
+                    </button>
+                    </div>
                 </div>
             </div>
         </div>
